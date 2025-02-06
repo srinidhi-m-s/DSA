c75f7ec6d780c10ac71bc69dfeef8c695f186fd0
Srinidhi Manohar <140908806+srinidhi-m-s@users.noreply.github.com>
2025-02-06 16:01:21 +0000

    Sync LeetCode submission - Replace Employee ID With The Unique Identifier (mysql)

--- my-folder/problems/replace_employee_id_with_the_unique_identifier/solution.sql
+++ my-folder/problems/replace_employee_id_with_the_unique_identifier/solution.sql
@@ -1,3 +1,3 @@
-# Write your MySQL query statement below
-select unique_id, name from employees e
-left join employeeuni u on e.id = u.id
+select EmployeeUNI.unique_id,name
+from Employees 
+left join EmployeeUNI on Employees.id=EmployeeUNI.id;
