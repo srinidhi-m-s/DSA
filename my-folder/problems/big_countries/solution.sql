aca740309d5daee0af4c6c9cb9e2a0206dad74e8
Srinidhi Manohar <140908806+srinidhi-m-s@users.noreply.github.com>
2025-02-06 15:23:00 +0000

    Sync LeetCode submission - Big Countries (mysql)

--- my-folder/problems/big_countries/solution.sql
+++ my-folder/problems/big_countries/solution.sql
@@ -1,2 +1,1 @@
-# Write your MySQL query statement below
-SELECT name, population, area FROM World where area >=3000000 or population >=25000000
+select name,population,area from World where area>=3000000 or population>=25000000;
