5ef0eb3ecb26a075ccb0241614b438c12c6e3ef9
Srinidhi Manohar <140908806+srinidhi-m-s@users.noreply.github.com>
2025-07-28 07:10:29 +0000

    Sync LeetCode submission - Valid Palindrome (java)

---
+++ my-folder/problems/valid_palindrome/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        
+        String a=s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+        System.out.println(a);
+        for(int i=0;i<a.length()/2;i++){
+            if(a.charAt(i)!=a.charAt(a.length()-1-i)) return false;
+        }
+        return true;
+    }
+}
